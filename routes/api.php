7994a597c30d8c526fcd2a27fd9176c4d4a8f22c
Sami-Regragui-Work <sami-regragui-work@protonmail.com>
2026-03-12 18:06:14 +0000

    feat(routes): added refresh route

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,7 @@ Route::post('login', [AuthController::class, 'login']);
 
 Route::middleware('auth:api')->group(function () {
     Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
     Route::get('me', [ProfileController::class, 'me']);
     Route::put('me', [ProfileController::class, 'update']);
     Route::put('me/password', [ProfileController::class, 'updatePassword']);
